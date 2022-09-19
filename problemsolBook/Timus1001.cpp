f733491d2cdf30d55d8d0e65833e95cdf4c2d5aa
Shuvo Islam Khan <shuvoislamkhan@gmail.com>
2022-09-20 02:08:02 +0600

    Mahbubul Hasan

--- problemsolBook/Timus1001.cpp
+++ problemsolBook/Timus1001.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #include <vector>
+#include<iomanip>
 using namespace std;
 int main() {
-  vector<long long>V;
-  long long a;
-  while((cin >> a) !=EOF){
-    V.pushback(a);
+  vector<double>V;
+  double a;
+
+  while(cin >> a &&a!=EOF){
+    V.push_back(a);
   }
   for(int i = V.size() -1; i>=0;i--){
-    cout << sqrt(V[i]) << endl;
+    cout<<setprecision(4)<<fixed<<sqrt(V[i])<<endl;
   }
   return 0;
 }
