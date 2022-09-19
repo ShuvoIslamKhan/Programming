37d618e0ee6c5b2a3b6c45aaa455c565a4b7bb25
Shuvo Islam Khan <shuvoislamkhan@gmail.com>
2022-09-20 02:07:54 +0600

    Mahbubul Hasan

---
+++ DataStructure/Timus1264.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+  long long N,M;;
+  cin >> N >> M;
+  cout << N*(M+1) << '\n';
+  return 0;
+}
---
+++ DataStructure/Timus1293.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int N,A,B;;
+  cin >> N >> A >> B;
+  cout << 2*N*A*B << '\n';
+  return 0;
+}
